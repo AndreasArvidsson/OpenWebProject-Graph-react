a71f651800071c59c8b75b1570fa4235cef9df8b
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2020-07-31 20:37:11 +0200

    Remove unsed webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,7 @@ module.exports = {
                     {
                         loader: "babel-loader",
                         options: {
-                            presets: ["@babel/preset-env", "@babel/preset-react"],
-                            plugins: [
-                                // "@babel/plugin-proposal-object-rest-spread",
-                                "@babel/plugin-proposal-class-properties",
-                                // "@babel/plugin-syntax-dynamic-import"
-                            ]
+                            presets: ["@babel/preset-env", "@babel/preset-react"]
                         }
                     },
                     {
