a5a76e9e662cb6ec6d5f0c278f4b66f82cf585e1
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2020-07-31 20:19:05 +0200

    Moved src file

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import Graph from "../src/index";
+import Graph from "..";
 
 const defaultOptions = {
     title: {
